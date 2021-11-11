f3167eb3d381501a96e4de3c66428bb75e114903
JasterV <jaster.victor@gmail.com>
2021-11-12 00:46:24 +0100

    adds constants

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,10 +1,12 @@
 import { Middleware } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 import { hmac } from "https://deno.land/x/god_crypto@v1.4.10/mod.ts";
 
+const SIGNATURE_HEADER = "X-Hub-Signature-256"
+
 export default (secret: string): Middleware =>
   async (ctx, next) => {
     const payload = await ctx.request.body().value;
-    const hash = ctx.request.headers.get("X-Hub-Signature-256");
+    const hash = ctx.request.headers.get(SIGNATURE_HEADER);
     const signature = "sha256=" +
       hmac("sha256", secret, JSON.stringify(payload || "")).hex();
     console.log("Signature", signature);
--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -2,6 +2,8 @@ import { Middleware } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 import { ActionsModel, ActionType } from "../types/mod.ts";
 import { execute, executeN } from "../lib/commands.ts";
 
+const GITHUB_EVENT = "x-github-event"
+
 export default (actionsModel: ActionsModel): Middleware =>
   async ({request, response}) => {
     if (!request.hasBody) {
@@ -15,7 +17,7 @@ export default (actionsModel: ActionsModel): Middleware =>
     console.log("Got request", { payload: body, headers });
     try {
       // Get commands from github event
-      const event = request.headers.get("x-github-event") || "";
+      const event = request.headers.get(GITHUB_EVENT);
       const commands = actionsModel.getCommands(event as ActionType);
       // Execute commands. If some command fails we log the error and reply back to github
       if (commands && Array.isArray(commands)) await executeN(commands);
