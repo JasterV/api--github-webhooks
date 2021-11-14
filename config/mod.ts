55b3cafff786be59a161b420a426b48f89513ae1
JasterV <jaster.victor@gmail.com>
2021-11-14 19:31:13 +0100

    changes port 8000

--- config/mod.ts
+++ config/mod.ts
@@ -3,7 +3,7 @@ import { config } from "../deps.ts";
 const { PORT, SECRET, ACTIONS_FILE_PATH } = config();
 
 const cnf = { 
-  port: PORT || Deno.env.get('PORT') || '8000', 
+  port: PORT || Deno.env.get('PORT'), 
   secret: SECRET || Deno.env.get('SECRET'),
   actionsFilePath: ACTIONS_FILE_PATH || Deno.env.get('ACTIONS_FILE_PATH') || '/actions.yaml'
 };
@@ -14,5 +14,6 @@ export default {
   get: (key: "port" | "secret" | "actionsFilePath") => cnf[key] || null,
   validate: () => {
     if (!cnf.secret) throw new Error("SECRET required but not found");
+    if (!cnf.port) throw new Error("PORT required but not found");
   },
 };
